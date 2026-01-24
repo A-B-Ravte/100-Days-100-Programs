eaecf46584e2bcdff7c667f59c20caf8fd8c1e87
A-B-Ravte <letsmailravte@gmail.com>
2026-01-24 19:56:00 +0530

    added code for day 04- agentic memory structure

---
+++ Day04/agentic_memory.py
@@ -0,0 +1,38 @@
+
+
+class AgenticAI:
+    def __init__(self, name: str):
+        self.name = name
+        self.memory: list[dict] = []
+
+    def add_memory(self, task_name:str , status:str):
+        record = {
+            "task":task_name,
+            "status":status
+        }
+        self.memory.append(record)
+        print(f"memory added for {self.name} : {task_name}")
+
+    def get_history(self)-> list: 
+        return self.memory
+
+    def clear_memory(self):
+        self.memory = []
+        print(f"Memory has been clear for {self.name}")
+        
+
+if __name__ == "__main__":
+    researcher = AgenticAI("Researhcer")
+    history = researcher.get_history()
+    print(f"History is {history}")
+    researcher.add_memory("Web Search", "Started")
+    
+    researcher.add_memory("Open Gmail", "Started")
+    
+    researcher.add_memory("Web Search", "Completed")
+
+    researcher.add_memory("Open Gmail", "Completed")
+    
+    history = researcher.get_history()
+    print(f"History is {history}")
+
