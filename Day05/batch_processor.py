bf47ef28689a97c0b0b1cca93b102870f56fefac
A-B-Ravte <letsmailravte@gmail.com>
2026-01-25 14:44:06 +0530

    Day 05 code added.

---
+++ Day05/batch_processor.py
@@ -0,0 +1,21 @@
+'''
+"The Batch Processor" (Program 05)? Here is the input to get started: raw_files = ["/home/user/logs/event1.json", "/data/config.yaml", "/var/log/event2.json", "/tmp/temp_data.txt", "direct_file.json"]
+
+The Goal: Use a single List Comprehension to get: ["processed_event1.json", "processed_event2.json", "processed_direct_file.json"]
+
+(Note: Don't forget that direct_file.json might not have a / in it—your code should handle that!)
+'''
+
+def batch_processor(raw_files:list)-> list :
+    json_files = []
+    for file in raw_files:
+        if file.rsplit('.')[-1].lower() == 'json':
+            file_name = "processed_" + file.split('/')[-1]
+            json_files.append(file_name)
+    return json_files        
+
+if __name__ == "__main__":
+    raw_files = ["/home/user/logs/event1.json", "/data/config.yaml", "/var/log/event2.json", "/tmp/temp_data.txt", "direct_file.json"]
+    json_files = batch_processor(raw_files)
+
+    print(json_files)
