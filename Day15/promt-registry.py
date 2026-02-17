16908670f09b62f58b5de127bfb54e65b5a87dc4
A-B-Ravte <letsmailravte@gmail.com>
2026-02-17 21:09:08 +0530

    Day15: Prompt-Registory Update - Refined if condition.

--- Day15/promt-registry.py
+++ Day15/promt-registry.py
@@ -10,11 +10,11 @@ prompt_registry={}
 
 def add_prompt(version_id: str, text: str, model_type: str, overwrite: bool = False):
     
-    if type(version_id) is not str or not version_id:
+    if not isinstance(version_id, str) or not version_id:
         logging.info(f"{version_id} must be string or not Empty")
         return
         
-    if prompt_registry.get(version_id) is not None and overwrite == False:
+    if prompt_registry.get(version_id) is not None and not overwrite:
             logging.info(f"Error: {version_id} already exists. Use overwrite=True to modify.")
     else:    
         prompt_registry[version_id] = {"text":text, "model":model_type}  
@@ -26,7 +26,7 @@ def get_prompt(version_id: str)-> dict:
         logging.info(f"{version_id} is not available")   
 
 if __name__ == "__main__":
-    add_prompt(version_id="", text="Summarize this: {text}", model_type="gpt-4")
+    add_prompt(version_id=1, text="Summarize this: {text}", model_type="gpt-4")
     add_prompt(version_id="v1.0", text="Shorten this: {text}", model_type="gpt-3.5")
     add_prompt(version_id="v1.0", text="Shorten this: {text}", model_type="gpt-3.5", overwrite=True)
     prompt =get_prompt("v1.0")
